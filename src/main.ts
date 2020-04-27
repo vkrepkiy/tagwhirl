8b5b732ec213c89be29dcac5064ad6dfe3d31a5f
Valentin <valkrepkiy@gmail.com>
2020-04-27 21:25:36 +0300

    add comments to code

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,39 @@
 /**
- * Class names
+ * TagWhirl is a Web Component which uses CSS animations, variables and Shadow DOM with slots
+ * to display a tag list with the whirl effect.
+ *
+ * Animation visualization may be customized by setting custom attributes.
+ * Available CSS variables:
+ * --link-color
+ * --link-hover-color
+ * --link-hover-background
+ *
+ * Template is included in file. HTML syntax can be highlighted in VScode
+ * with 'es6-string-html' extension.
  */
-const titleClassName = "title";
+
+/**
+ * List of all constants for classes, ids and selectors for template and code.
+ */
 const ulClassName = "listContainer";
-const liTemplateId = "listItemId";
-const liClassName = "listItem";
-const linkClassName = "listItemLink";
-
-/**
- * Selectors
- */
-const itemWrapperSelector = `.${ulClassName}`;
-const itemSelector = `.${liClassName}`;
-const itemLinkSelector = `.${linkClassName}`;
-const itemTplSelector = `#${liTemplateId}`;
+const ulSelector = `.${ulClassName}`;
+const titleClassName = "title";
 const titleSelector = `.${titleClassName}`;
+const liClassName = "listItem";
+const liSelector = `.${liClassName}`;
+const liTemplateId = "listItemId";
+const liTemplateSelector = `#${liTemplateId}`;
+const linkClassName = "listItemLink";
+const linkSelector = `.${linkClassName}`;
+const animationStyleId = "generatedAnimation";
+const animationStyleSelector = "#generatedAnimation";
+const titleSlotName = "title";
 
 /**
- * Create template node.
- * Syntax highlighting for VSCode is available with 'es6-string-html' extension.
+ * Template for {@link TagWhirl} custom element.
+ *
+ * @remarks
+ * Part of stylesheet is generated at runtime in style tag with selector {@link animationStyleSelector}
  */
 const template = document.createElement("template");
 template.innerHTML = /*html*/ `
@@ -59,8 +74,8 @@ template.innerHTML = /*html*/ `
     }
     .${linkClassName} {
       position: relative;
-      color: var(--text-color);
-      text-decoration: none;
+      color: var(--link-color);
+      text-decoratio: none;
       display: block;
       transform: translateX(-50%) translateY(-50%);
       padding: 0.2em;
@@ -74,29 +89,39 @@ template.innerHTML = /*html*/ `
     }
     .${linkClassName}:hover {
       cursor: pointer;
-      color: var(--color-accent);
-      background: black;
+      color: var(--link-hover-color);
+      background: var(--link-hover-background);
       font-size: 1.5em;
     }
   </style>
-  <style>
-    /* here will be generated css */
-  </style>
+  <style id="${animationStyleId}"></style>
   <h1 class="${titleClassName}">
-    <slot name="title"></slot>
+    <slot name="${titleSlotName}"></slot>
   </h1>
   <ul class="${ulClassName}"></ul>
-
   <template id="${liTemplateId}">
     <li class="${liClassName}"><a class="${linkClassName}"></a></li>
   </template>
 `;
 
+/**
+ * This interface describes single tag item.
+ */
 export interface TagOption {
+  /**
+   * This text is used to display tag.
+   */
   text: string;
+  /**
+   * Link is optional.
+   */
   href?: string;
 }
 
+/**
+ * Each tag item has it's own animation config.
+ * @internal
+ */
 interface AnimationConfig {
   fontSizeEm: number;
   secForTurn: number;
@@ -104,12 +129,33 @@ interface AnimationConfig {
   translateX: number;
 }
 
+/**
+ * The main class for tag whirl custom element.
+ */
 export class TagWhirl extends HTMLElement {
+  /**
+   * Define how much turns whirl should have.
+   */
   public turns: number = 3;
+
+  /**
+   * How long it takes a tag to make a turn.
+   */
   public secForTurn: number = 50;
+
+  /**
+   * Font size minimal value.
+   */
   public fontSizeMinEm: number = 0.7;
+
+  /**
+   * Font size reduce ratio.
+   */
   public fontSizeRatio: number = 1.5;
 
+  /**
+   * Main property, it accepts TagOptions to build tag whirl.
+   */
   public set items(value: TagOption[]) {
     this.setItems(value);
   }
@@ -117,26 +163,46 @@ export class TagWhirl extends HTMLElement {
     return this.currentItems;
   }
 
+  /**
+   * Contain ordered list of tags.
+   */
   private currentItems: TagOption[] = [];
 
+  /**
+   * Shadow DOM root element.
+   */
   private get rootEl(): ShadowRoot {
     return this.shadowRoot || this.attachShadow({ mode: "open" });
   }
 
-  private containerMinSizePx: number = 400;
+  /**
+   * Minium container size is calculated in {@link TagWhirl.updateSize}.
+   */
+  private minSizePx: number = 0;
 
+  /**
+   * Title size is calculated in {@link TagWhirl.updateTitle}.
+   */
   private titleSizePx: number = 0;
 
+  /**
+   * Part of custom element specification, required for {@link TagWhirl.attributeChangedCallback}.
+   *
+   * sec-for-turn - {@link TagWhirl.secForTurn}
+   * font-size-min-em - {@link TagWhirl.fontSizeMinEm}
+   * font-size-ratio - {@link TagWhirl.fontSizeRatio}
+   */
   static get observedAttributes(): string[] {
-    return [
-      "items",
-      "turns",
-      "sec-for-turn",
-      "font-size-min-em",
-      "font-size-multiplier",
-    ];
+    return ["turns", "sec-for-turn", "font-size-min-em", "font-size-ratio"];
   }
 
+  /**
+   * Part of custom element specification.
+   *
+   * @param name one of observed attributes
+   * @param oldValue
+   * @param newValue
+   */
   public attributeChangedCallback(
     name: string,
     oldValue: string,
@@ -159,7 +225,7 @@ export class TagWhirl extends HTMLElement {
         this.fontSizeMinEm = +newValue;
         this.updateStyleSheet();
         break;
-      case "font-size-multiplier":
+      case "font-size-ratio":
         this.fontSizeMinEm = +newValue;
         this.updateStyleSheet();
         break;
@@ -168,6 +234,9 @@ export class TagWhirl extends HTMLElement {
     }
   }
 
+  /**
+   * Part of custom element specification.
+   */
   public connectedCallback(): void {
     this.rootEl.appendChild(template.content.cloneNode(true));
 
@@ -175,44 +244,58 @@ export class TagWhirl extends HTMLElement {
     this.updateSize();
   }
 
+  /**
+   * Set all tags
+   *
+   * @param value tag options
+   */
   private setItems(value: TagOption[]): void {
     this.currentItems = this.normalizeValue(value);
     this.updateDomNodes();
     this.updateStyleSheet();
   }
 
+  /**
+   * Update custom element minimal size. Animation styles are not recalculated.
+   */
   private updateSize(): void {
-    this.containerMinSizePx = Math.min(this.offsetHeight, this.offsetWidth);
+    this.minSizePx = Math.min(this.offsetHeight, this.offsetWidth);
   }
 
+  /**
+   * Update title size. Animation styles are not recalculated.
+   */
   private updateTitle(): void {
     const titleEL = this.rootEl.querySelector(titleSelector) as HTMLElement;
 
     this.titleSizePx = Math.max(titleEL?.clientHeight, titleEL.clientWidth);
   }
 
+  /**
+   * Prepare tag options for rendering. Sort tags by text length.
+   */
   private normalizeValue(value: TagOption[]): TagOption[] {
     return value.sort((a, b) =>
       !!a && !!b ? a.text.length - b.text.length : 0
     );
   }
 
+  /**
+   * Clear tag list and append all tags to HTML (without animation stylesheet).
+   */
   private updateDomNodes(): void {
-    const wrapper = this.rootEl.querySelector(
-      itemWrapperSelector
-    ) as HTMLElement;
+    const wrapper = this.rootEl.querySelector(ulSelector) as HTMLElement;
     const tpl = this.rootEl.querySelector(
-      itemTplSelector
+      liTemplateSelector
     ) as HTMLTemplateElement;
 
     wrapper.innerHTML = "";
 
     for (let i = 0; i < this.currentItems.length; i++) {
       const tplClone = tpl.content.cloneNode(true) as DocumentFragment;
-      const link = tplClone.querySelector(
-        itemLinkSelector
-      ) as HTMLAnchorElement;
+      const link = tplClone.querySelector(linkSelector) as HTMLAnchorElement;
       link.textContent = this.currentItems[i].text;
+
       if (!!this.currentItems[i].href) {
         link.href = this.currentItems[i].href!;
       }
@@ -220,13 +303,16 @@ export class TagWhirl extends HTMLElement {
     }
   }
 
+  /**
+   * Clear animation stylesheet, generate animation configs and append new stylesheet.
+   */
   private updateStyleSheet(): void {
     const configs: AnimationConfig[] = this.getConfigs(
       this.currentItems.length
     );
-    const styleSheet = this.rootEl.styleSheets.item(
-      this.rootEl.styleSheets.length - 1
-    ) as CSSStyleSheet;
+    const styleSheet = (this.rootEl.querySelector(
+      animationStyleSelector
+    ) as HTMLStyleElement).sheet as CSSStyleSheet;
 
     for (let i = styleSheet.cssRules.length - 1; i >= 0; i--) {
       styleSheet.removeRule(i);
@@ -236,7 +322,7 @@ export class TagWhirl extends HTMLElement {
       const config = configs[i];
       const nthChild = i + 1;
 
-      styleSheet.insertRule(`${itemSelector}:nth-child(${nthChild}) {
+      styleSheet.insertRule(`${liSelector}:nth-child(${nthChild}) {
         font-size: ${config.fontSizeEm}em;
         animation: rotate_${nthChild} ${config.secForTurn}s infinite linear, appear_${nthChild} ${config.rotationOffset}ms 1 forwards;
         animation-delay: ${config.rotationOffset}ms;
@@ -267,7 +353,7 @@ export class TagWhirl extends HTMLElement {
   }
 
   /**
-   * Get animated items and configs
+   * Get required amount of configs
    */
   private getConfigs(amount: number): AnimationConfig[] {
     const configs: AnimationConfig[] = [];
@@ -277,6 +363,12 @@ export class TagWhirl extends HTMLElement {
     return configs;
   }
 
+  /**
+   * Get single config
+   *
+   * @param total total count of required configs
+   * @param i current config index
+   */
   private getRandomConfig(total: number, i: number): AnimationConfig {
     // Generate config based on index to total ratio
     const randomSeed = i / total;
@@ -287,13 +379,11 @@ export class TagWhirl extends HTMLElement {
       translateX: Math.round(
         Math.max(
           this.titleSizePx,
-          Math.round(randomSeed * (this.containerMinSizePx / 2))
+          Math.round(randomSeed * (this.minSizePx / 2))
         )
       ),
     };
   }
 }
-
-customElements.define("vk-skill-whirl", TagWhirl);
 
 export default TagWhirl;
